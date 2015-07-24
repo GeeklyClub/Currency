aad4e705d8403ff81ea595651b067790d25b2ac9
-_-! <nonmadden@gmail.com>
2015-07-24 19:29:13 +0700

    Test heroku

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,27 @@
 class WelcomeController < ApplicationController
 
   def index
+    data = SmarterCSV.process('public/rms_five.csv')
 
+    from  = params[:from]
+    to    = params[:to]
+
+    unless from.nil? or to.nil?
+      # date = Date.yesterday.strftime("%d/%m//%Y")
+      @from = data.select { |data| data[:currency] == from }[0][:"23/07/2015"]
+      @to   = data.select { |data| data[:currency] == to }[0][:"23/07/2015"]
+      @exchange = @from / @to
+    else
+      from = from.nil? ? "Thai Baht" : from
+
+      @from = data.select { |data| data[:currency] == from }[0][:currency]
+
+
+      # raise @from.inspect
+      @exchange = []
+      data.each do |d|
+        @exchange << { currency: d[:currency], value: @from.to_f / d[:"23/07/2015"].to_f }
+      end
+    end
   end
 end
