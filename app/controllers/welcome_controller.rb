84893edadbc3e7ad4c03f70f3f1c9a0bd42dbef0
-_-! <non@page365.net>
2015-07-27 15:16:18 +0700

    Add ISO Code

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,8 +6,12 @@ class WelcomeController < ApplicationController
     from = data[60..110].select { |data| data[0] == from }[0][2]
 
     @exchange = []
-    data[60..110].each do |d|
-      @exchange << { currency: d[0], value: d[2].to_f / from.to_f }
+    data[60..110].each_with_index do |d, index|
+      @exchange << { currency: d[0], value: d[2].to_f / from.to_f, iso_code: iso_code[index] }
     end
+  end
+
+  def iso_code
+    %w(EUR JPY GBP USD DZD AUD BHD BWP BRL BND CAD CLP CNY COP CZK DKK HUF ISK INR IDR IRR ILS KZT KRW KWD LYD MYR MUR MXN NPR NZD NOK OMR PKR PEN PHP PLN QAR RUB SAR SGD ZAR LKR SEK CHF THB TTD TND AED ARS VEF)
   end
 end
