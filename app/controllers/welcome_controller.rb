8baf8d3e6f7ed4d9a4e7337c94f26e3dfe449a34
-_-! <non@page365.net>
2015-07-27 12:37:59 +0700

    fix calculation

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,12 +2,12 @@ class WelcomeController < ApplicationController
 
   def index
     data = CSV.read(Rails.root.join("public/rms.xls"), { :col_sep => "\t" })
-    from = params[:from] || "Thai Bath"
+    from = params[:from] || "Thai Baht"
     from = data[60..110].select { |data| data[0] == from }[0][2]
 
     @exchange = []
     data[60..110].each do |d|
-      @exchange << { currency: d[0], value: d[3].to_f / from.to_f }
+      @exchange << { currency: d[0], value: d[2].to_f / from.to_f }
     end
   end
 end
