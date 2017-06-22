798c1acdad6c8df6a2d8904c1abb9cde6ecc286c
Arnon Hongklay <arnon@hongklay.com>
2017-06-22 19:42:42 +0700

    upgrade source rails 5.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  root 'welcome#index', defaults: { format: :json }
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
