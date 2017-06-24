9afc05740d2c026312fbdd433cb6dff785207269
Arnon Hongklay <arnon@hongklay.com>
2017-06-24 17:47:05 +0700

    upgrade gem

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,38 +1,38 @@
 class WelcomeController < ApplicationController
 
   def index
-    datas = CSV.read(Rails.root.join("public/rms.xls"), { :col_sep => "\t" })
+    # datas = CSV.read(Rails.root.join("public/rms.xls"), { :col_sep => "\t" })
 
-    loop_start = 0
-    loop_end = 0
+    # loop_start = 0
+    # loop_end = 0
 
-    datas.each_with_index do |data, index|
-      case data.first
-      when /Currency/
-        loop_start = index + 1
-      when /\(1\) Exchange rates are published daily except on IMF holidays or/
-        loop_end = index - 3
-      end
-    end
+    # datas.each_with_index do |data, index|
+    #   case data.first
+    #   when /Currency/
+    #     loop_start = index + 1
+    #   when /\(1\) Exchange rates are published daily except on IMF holidays or/
+    #     loop_end = index - 3
+    #   end
+    # end
 
-    datas = datas.values_at(loop_start..loop_end)
-    datas.push(["Vietnamese dong", nil, 1 / 0.00003278, "VND"])
-    datas.each_with_index { |data| data.push(currency_iso_code[data.first.to_s.split.join.downcase]) }
+    # datas = datas.values_at(loop_start..loop_end)
+    # datas.push(["Vietnamese dong", nil, 1 / 0.00003278, "VND"])
+    # datas.each_with_index { |data| data.push(currency_iso_code[data.first.to_s.split.join.downcase]) }
 
-    from = params[:from] || "THB"
-    from = datas.select { |data| data.last == from.upcase }
-    return if from.empty?
-    from = from.first.third || from.first.fourth
+    # from = params[:from] || "THB"
+    # from = datas.select { |data| data.last == from.upcase }
+    # return if from.empty?
+    # from = from.first.third || from.first.fourth
 
-    @exchange = []
-    datas.each_with_index do |data, index|
-      to = data.third || data.fourth
-      @exchange << {
-        currency: data.first,
-        iso_code: data.last,
-        value: to.to_s.gsub(/[^\d^\.]/, '').to_f / from.to_s.gsub(/[^\d^\.]/, '').to_f,
-      }
-    end
+    # @exchange = []
+    # datas.each_with_index do |data, index|
+    #   to = data.third || data.fourth
+    #   @exchange << {
+    #     currency: data.first,
+    #     iso_code: data.last,
+    #     value: to.to_s.gsub(/[^\d^\.]/, '').to_f / from.to_s.gsub(/[^\d^\.]/, '').to_f,
+    #   }
+    # end
   end
 
   def exchange
