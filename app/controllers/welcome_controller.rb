3965a95ac68fec2fba5aaa568acdfffc9d8ea39d
Arnon Hongklay <arnon@hongklay.com>
2017-06-24 15:06:17 +0700

    exchange page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -17,7 +17,7 @@ class WelcomeController < ApplicationController
 
     datas = datas.values_at(loop_start..loop_end)
     datas.push(["Vietnamese dong", nil, 1 / 0.00003278, "VND"])
-    datas.each_with_index { |data| data.push(iso_code[data.first.to_s.split.join.downcase]) }
+    datas.each_with_index { |data| data.push(currency_iso_code[data.first.to_s.split.join.downcase]) }
 
     from = params[:from] || "THB"
     from = datas.select { |data| data.last == from.upcase }
@@ -35,7 +35,44 @@ class WelcomeController < ApplicationController
     end
   end
 
-  def iso_code
+  def exchange
+    datas = CSV.read(Rails.root.join("public/rms.xls"), { :col_sep => "\t" })
+
+    loop_start = 0
+    loop_end = 0
+
+    datas.each_with_index do |data, index|
+      case data.first
+      when /Currency/
+        loop_start = index + 1
+      when /\(1\) Exchange rates are published daily except on IMF holidays or/
+        loop_end = index - 3
+      end
+    end
+
+    datas = datas.values_at(loop_start..loop_end)
+    datas.push(["Vietnamese dong", nil, 1 / 0.00003278, "VND"])
+    datas.each_with_index { |data| data.push(currency_iso_code[data.first.to_s.split.join.downcase]) }
+
+    @selection = currency_iso_code.to_a
+
+    from = params[:from] || "THB"
+    from = datas.select { |data| data.last == from.upcase }
+    return if from.empty?
+    from = from.first.third || from.first.fourth
+
+    @exchange = []
+    datas.each_with_index do |data, index|
+      to = data.third || data.fourth
+      @exchange << {
+        currency: data.first,
+        iso_code: data.last,
+        value: to.to_s.gsub(/[^\d^\.]/, '').to_f / from.to_s.gsub(/[^\d^\.]/, '').to_f,
+      }
+    end
+  end
+
+  def currency_iso_code
     # ref http://www.oanda.com/help/currency-iso-code-country#U
     {
       "euro" =>                     "EUR",
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index', defaults: { format: :json }
+  root 'welcome#index'
+  get '/exchange', to: 'welcome#exchange'
+  get '/currency_iso_code', to: 'welcome#currency_iso_code', format: :json
 end
